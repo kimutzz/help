601d14fc9b92d4488317894fb7fccfe4925f8fbc
kimutzz <73109280+kimutzz@users.noreply.github.com>
2022-03-23 18:51:24 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,12 @@
+<header id="header">
+    <hgroup id="title">
+      <div id="logo"><center><img src="https://i.gifer.com/origin/df/df2194fd5e3df54b35dff3b540daefc7_w200.gif" width=10%/></centre></div>
+      <h2 style="font-family:serif;">WHAT'S <small>The</small> EMERGENCY</h2>
+    </hgroup>
+    <nav class="navbar">
+	<a href="index.html"><button class="btn btn-danger">Home</button></a> 
+	<!--a href="login.html">Sign in</a-->
+	<a href="contact.html"><button class="btn btn-danger">Contact Us</button></a>
+	<!--a href="logout.php">Logout</a-->
+	</nav>
+  </header>
